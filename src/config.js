3189582218201768408381d04415280f1c5df17d
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-07-08 13:21:15 +0530

    backend domain changes

--- src/config.js
+++ src/config.js
@@ -4,9 +4,17 @@
 
 
 
-//   VITE_API_URL="https://ces-backend-aczm.onrender.com";
+// //   VITE_API_URL="https://ces-backend-aczm.onrender.com";
 
-VITE_API_URL = "https://api.circuitses.com"
+// VITE_API_URL = "https://api.circuitses.com"
 
 
-export const API_URL = import.meta.env.VITE_API_URL;
+// export const API_URL = import.meta.env.VITE_API_URL;
+
+
+// export const API_URL =
+//   "https://ces-backend-aczm.onrender.com";
+
+
+  export const API_URL =
+  "https://api.circuitses.com";
