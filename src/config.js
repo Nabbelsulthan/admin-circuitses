44d74fc16637e95f1387e55cc96287120212f1f3
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-07-08 13:12:46 +0530

    live api changes

--- src/config.js
+++ src/config.js
@@ -1,2 +1,12 @@
-export const API_URL =
-  "https://ces-backend-aczm.onrender.com";
+
+
+
+
+
+
+//   VITE_API_URL="https://ces-backend-aczm.onrender.com";
+
+VITE_API_URL = "https://api.circuitses.com"
+
+
+export const API_URL = import.meta.env.VITE_API_URL;
